ce3b2764005cda14cd50f85f7932d5c9237dedc4
Kaustubh Kadam <88819132+KadamKaustubh147@users.noreply.github.com>
2026-05-13 23:58:33 +0530

    Time: 9 ms (44.29%), Space: 19.3 MB (61.52%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
@@ -1,11 +1,24 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        time = 0
+        q = deque()
+        # q.append(for ticket in tickets[::-1])
+        for ticket in tickets:
+            q.append(ticket)
+        # n = len(tickets)
+        time = 0 # or the number of iterations
 
-        for i,num in enumerate(tickets):
-            if i<=k:
-                time += min(tickets[i], tickets[k])
+        while q:
+            front = q.popleft()
+            front -= 1
+            time += 1
+            if front==0 and k==0:
+                return time
+            if front != 0:
+                q.append(front)
+            if k==0:
+                k=len(q)-1
             else:
-                time += min(tickets[i], tickets[k]-1)
+                k-=1
 
         return time
+            
